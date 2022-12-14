3ce4c6aaced15d1cd31a4359952b5d376bc8001e
KrzysztofHum <k.humienny1996@protonmail.com>
2022-12-14 14:11:13 +0100

    filder architecture and react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./scenes/homePage";
+import LoginPage from "./scenes/LoginPage";
+import ProfilePage from "./scenes/profilePage";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
